d4ed695eeb87a3394cadef091cfe3e8cb3ae27b3
omerfarukkdogan <omerc@DESKTOP-P7FDVCL>
2024-05-26 11:54:59 +0300

    Add project files.

---
+++ QuizApplication.Backend/Context/QuizAppVeriTabaniBaglantisi.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuizApplication.Backend.Models;
+
+namespace QuizApplication.Backend.Context;
+
+public class QuizAppVeriTabaniBaglantisi : DbContext
+{
+
+    public QuizAppVeriTabaniBaglantisi(DbContextOptions opt) : base(opt)
+    {
+        
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB; Database=Quiz_app_db; Trusted_Connection= true;");
+    }
+
+
+    public DbSet<Question> Questions { get; set; }
+    public DbSet<Answer> Answers { get; set; }
+
+    // add-migration <isim>: .net tarafında değişiklikleri algılar 
+    // update-database : görülen değişikliği uygular.
+}
---
+++ QuizApplication.Backend/Models/Answer.cs
@@ -0,0 +1,14 @@
+﻿namespace QuizApplication.Backend.Models;
+
+public class Answer
+{
+    public int Id { get; set; }
+
+    public string Text { get; set; }
+
+    public bool IsSuccess { get; set; }
+
+    public int QuestionId { get; set; }
+
+    public Question Question { get; set; }
+}
